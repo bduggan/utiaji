ff44dc8b71b41ce485846c5439d80f8e208f0ca4
Brian Duggan <brian@promptworks.com>
2016-01-15 16:00:36 -0500

    cache perl6 and panda (merge cache_perl6 branch)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Utiaji
+Utiaji -- personal information organizer
 ======
 [![Build Status](https://travis-ci.org/bduggan/utiaji.svg?branch=master)](https://travis-ci.org/bduggan/utiaji)
+
+Description
+===========
+Utiaji is a calender, wiki and personal information organizer.
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -ex
+
+version=2015.12
+
+if [ -d $HOME/build_cache/moar-$version ]; then
+    rm -rf $HOME/.rakudobrew/moar-$version
+    cp -al $HOME/build_cache/moar-$version $HOME/.rakudobrew
+    rakudobrew global $version
+    rakudobrew rehash
+else
+    rakudobrew build moar $version
+    rakudobrew build-panda
+    cp -al $HOME/.rakudobrew/moar-$version $HOME/build_cache
+fi
+
+panda installdeps .
+
