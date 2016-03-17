b851aabf9cea62d123691219c7816d339df75efa
Brian Duggan <brian@promptworks.com>
2016-03-17 12:00:42 -0400

    reinstall each time

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ version=2016.01
 if [ -x $HOME/.rakudobrew/bin/perl6 ]; then
     rakudobrew global $version
     rakudobrew rehash
-    panda installdeps .
+    panda --notests install DBIish
+    panda install HTTP::Tinyish
     exit 0
 fi
 
@@ -13,7 +14,6 @@ rm -rf $HOME/.rakudobrew
 git clone https://github.com/tadzik/rakudobrew.git $HOME/.rakudobrew
 rakudobrew build moar $version
 rakudobrew build-panda
-# panda installdeps .
 panda --notests install DBIish
 panda install HTTP::Tinyish
 
