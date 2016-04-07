e03233460930e26fb0d5fbbffbb7aaa8dbc36d78
Brian Duggan <brian@promptworks.com>
2016-04-07 17:02:21 -0400

    typo

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@
 
 version=2016.03
 
-if [ -x $HOME/.rakudobrew/bin/perl6 && $HOME/.rakudobrew/bin/perl6 --version | grep -q $version ]; then
-    echo "using $version"
-    rakudobrew global $version
-    rakudobrew rehash
-    panda --notests install DBIish
-    panda install HTTP::Tinyish
-    exit
+if [ -x $HOME/.rakudobrew/bin/perl6 ]; then
+    if [ $HOME/.rakudobrew/bin/perl6 --version | grep -q "$version" ]; then
+        echo "using $version"
+        rakudobrew global $version
+        rakudobrew rehash
+        panda --notests install DBIish
+        panda install HTTP::Tinyish
+        exit
+    fi
 fi
 
 echo "building $version"
