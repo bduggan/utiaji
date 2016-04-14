5f50ff3c14a660e00e8dff7cc77c74d86156464c
Brian Duggan <brian@promptworks.com>
2016-04-13 23:42:19 -0400

    wiki

--- static/pim/pim.js
+++ static/pim/pim.js
@@ -4,16 +4,15 @@ var Wiki = React.createClass({
 
     getInitialState: function() {
         return {
-            editing: 0,
+            editing: (this.props.initial_text ? false : true),
             text: this.props.initial_text
         }
     },
 
     save: function() {
         var t = this.state.text;
-        console.log('saving ' + t);
         var url = window.location.href;
-        console.log('to ' + url);
+        var that = this;
         fetch(url, {
             method: 'post',
             headers: { 'content-type':'application/json'},
@@ -21,6 +20,7 @@ var Wiki = React.createClass({
         })
         .then(function(data){
             console.log('got ', data)
+            that.setState({ text: t, editing: false })
         })
         .catch(function(err) {
             console.log('error ' + err);
@@ -31,16 +31,24 @@ var Wiki = React.createClass({
         this.setState({ text: e.target.value } );
     },
 
+    editMode: function(e) {
+        this.setState({ text: this.state.text, editing: true } );
+    },
     render: function () {
         return (
             el('div', {},
                 el('row', {},
                     el('div', { className: 'text-right' },
-                        el('a', { className: 'button', onClick: this.save },'save' )
+                        this.state.editing ?
+                        el('a', { className: 'button', onClick: this.save },'save' ) :
+                        el('a', { className: 'button', onClick: this.editMode },'edit' )
                       )
                    ),
+                   this.state.editing ?
                    el('textarea', { id: 'note', onChange: this.handleChange,
                        placeholder: 'New Page', rows: 19, value: this.state.text })
+                   :
+                   el('pre', {}, this.state.text)
               )
         )
     }
