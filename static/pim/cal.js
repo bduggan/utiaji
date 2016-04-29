b298e63a1b8e9d753bc4e797e4b3f4c9a00c5f35
Brian Duggan <bduggan@matatu.org>
2016-04-29 14:40:48 -0400

    dates

--- static/pim/cal.js
+++ static/pim/cal.js
@@ -1,5 +1,5 @@
 
-['div','table','tbody','tr','th','td']
+['div','table','tbody','tr','th','td','span']
   .map(function(v) {
     eval( v + " = gen('" + v + "');");
 });
@@ -9,7 +9,15 @@ var Cal = React.createClass({
     getInitialState: function() {
         return {
             month: 'April',
-            year: '2016'
+            year: '2016',
+            dates: [
+                27,28,29,30,31, 1, 2,
+                 3, 4, 5, 6, 7, 8, 9,
+                10,11,12,13,14,15,16,
+                17,18,19,20,21,22,23,
+                24,25,26,27,28,29,30,
+                 1, 2, 3, 4, 5, 6, 7
+                ]
         }
     },
 
@@ -24,12 +32,12 @@ var Cal = React.createClass({
                 tbody(
                     ...tr([
                         th( ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'] ),
-                        td( ['0', '1', '2', '3', '4', '5', '6' ] ),
-                        td( ['0', '1', '2', '3', '4', '5', '6' ] ),
-                        td( ['0', '1', '2', '3', '4', '5', '6' ] ),
-                        td( ['0', '1', '2', '3', '4', '5', '6' ] ),
-                        td( ['0', '1', '2', '3', '4', '5', '6' ] ),
-                        td( ['0', '1', '2', '3', '4', '5', '6' ] )
+                        td( span( {className:'dt'}, this.state.dates.slice(0,7) ) ),
+                        td( span( {className:'dt'}, this.state.dates.slice(7,14) ) ),
+                        td( span( {className:'dt'}, this.state.dates.slice(14,21) ) ),
+                        td( span( {className:'dt'}, this.state.dates.slice(21,28) ) ),
+                        td( span( {className:'dt'}, this.state.dates.slice(28,35) ) ),
+                        td( span( {className:'dt'}, this.state.dates.slice(35,42) ) )
                        ])
                      )
                  )
