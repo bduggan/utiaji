62bfef6bdc9a54341306d1f34f3d13958331dda5
Brian Duggan <bduggan@matatu.org>
2016-05-31 14:24:20 -0400

    wiki

--- static/pim/wiki.js
+++ static/pim/wiki.js
@@ -11,19 +11,31 @@ var Wiki = React.createClass({
             txt: txt,
             dates: dates,
             version: 1,
-            last_save: 1
+            last_save: 1,
+            last_touch: new Date().getTime()
         }
     },
 
     componentDidMount: function() {
-        setInterval(this.maybeSave,1500)
+        setInterval(this.maybeSave,1000)
     },
 
     maybeSave: function() {
-        if (!this.is_modified()) {
+        if (this.is_modified()) {
+            this.save();
             return;
         }
-        this.save();
+        if (this.state.editing && this.state.txt && this.elapsed(3000)) {
+            console.log('stop editing');
+            this.setState({editing: false});
+        }
+        return;
+    },
+
+    elapsed: function(t) {
+        var e = new Date().getTime() - this.state.last_touch;
+        console.log('elapsed: ',e);
+        return e > t;
     },
 
     save: function() {
@@ -54,8 +66,13 @@ var Wiki = React.createClass({
         })
     },
 
+    touch: function() {
+        this.setState({ last_touch: new Date().getTime() });
+    },
+
     editMode: function(e) {
-        this.setState({ txt: this.state.txt, editing: true } );
+        this.setState({ editing: true } );
+        this.touch();
     },
 
     is_modified: function() {
@@ -72,9 +89,15 @@ var Wiki = React.createClass({
                 div( { className: 'status-indicator ' + (this.is_modified() ? 'changed' : 'saved') }, ''),
                 row(
                     this.state.editing ?
-                    textarea( { id: 'note', onChange: this.handleChange,
-                        placeholder: 'New Page (use @link to make links)',
-                        rows: 19, value: this.state.txt })
+                    textarea(
+                        {
+                            autoFocus: true,
+                            id: 'note',
+                            onChange: this.handleChange,
+                            onKeyDown: this.touch,
+                            placeholder: 'New Page (use @link to make links)',
+                            rows: 19, value: this.state.txt
+                        })
                     : pre({
                         className: 'secondary callout',
                         onClick: this.editMode,
