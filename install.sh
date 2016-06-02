ebf3f1d545e5344848998faf1efbd895704590cc
Brian Duggan <bduggan@matatu.org>
2016-06-02 12:26:19 -0400

    panda

--- install.sh
+++ install.sh
@@ -3,13 +3,18 @@
 version=2016.05
 
 export PATH=$HOME/.rakudobrew/bin:$PATH
+
+deps() {
+    panda --installed list | grep -q DBIish || panda --notests install DBIish
+    panda --installed list | grep -q 'HTTP::Tinyish' || panda install HTTP::Tinyish
+}
+
 if [ -x $HOME/.rakudobrew/bin/perl6 ]; then
     if $HOME/.rakudobrew/bin/perl6 --version | grep -q "$version"; then
         echo "using $version"
         rakudobrew global $version
         rakudobrew rehash
-        panda --notests install DBIish
-        panda install HTTP::Tinyish
+        install_deps()
         exit
     fi
 fi
@@ -20,6 +25,4 @@ rm -rf $HOME/.rakudobrew
 git clone https://github.com/tadzik/rakudobrew.git $HOME/.rakudobrew
 rakudobrew build moar $version
 rakudobrew build-panda
-panda --installed list | grep -q DBIish || panda --notests install DBIish
-panda --installed list | grep -q 'HTTP::Tinyish' || panda install HTTP::Tinyish
-
+install_deps()
