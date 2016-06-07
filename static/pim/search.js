628da2106b0b4ae4f8c60df300e6d8723c920a60
Brian Duggan <bduggan@matatu.org>
2016-06-06 20:42:36 -0400

    debounce and check cache on render
    thanks for the ideas, @ryanhinkel

--- static/pim/search.js
+++ static/pim/search.js
@@ -1,9 +1,10 @@
 use_tags(['input','div','ul','li','a']);
-var search_cache = {};
+var timeout;
 var Search = React.createClass({
     getInitialState: function() {
         var state = {
             query: "",
+            search_cache : {},
             results: [ ]
                 //{ href: '/cal/x', label: 'one' },
                 //{ href: '/wiki/', label: 'two' },
@@ -17,31 +18,54 @@ var Search = React.createClass({
         }
     },
     clear: function() {
-        this.setState({ results : [], query: "" });
+        this.setState({ txt: "", results : [], query: "" });
+    },
+    cached: function(v) {
+        return this.state.search_cache[v];
+    },
+    add_to_cache: function(k,v) {
+        var cache = this.state.search_cache;
+        cache[k] = v;
+        this.setState({ search_cache : cache });
     },
     handleChange: function(e) {
         var query = e.target.value;
         if (!query.length) {
             return this.clear();
         }
-        if (search_cache[query]) {
-            this.setState({ query: query, results: search_cache[query] });
+        this.setState({txt:query});
+        if (c = this.cached(query)) {
+            this.setState({ query: query, results: c });
             return;
         }
         var that = this;
-        fetch( '/search', {
-            method: 'POST',
-            headers: { 'Content-Type' : 'application/json' },
-            body: JSON.stringify({txt: query})
-        }).then(function(response){
-            return response.json();
-        }).then(function(results) {
-            search_cache[query] = results;
-            that.setState({ query: query, results: results })
-        })
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(function() {
+            fetch( '/search', {
+                method: 'POST',
+                headers: { 'Content-Type' : 'application/json' },
+                body: JSON.stringify({txt: query})
+            }).then(function(response){
+                return response.json();
+            }).then(function(results) {
+                that.add_to_cache(query,results);
+                that.setState({ query: query, results: results })
+            })
+        },250);
     },
     render: function() {
         var s = this.state;
+        var query;
+        var results;
+        if (c = this.cached(s.txt)) {
+            query = s.txt;
+            results = c;
+        } else {
+            query = s.query;
+            results = s.results;
+        }
         return div( { className:'searchbox' },
             input( { className: 'search',
                 type: 'text',
@@ -50,11 +74,11 @@ var Search = React.createClass({
                 autocomplete: 'off',
                 onKeyDown: this.handleKeys,
                 onChange: this.handleChange } ),
-            (s.results.length==0 ? undefined :
+            (query.length==0 ? undefined :
             div({className: 'searchresults'},
                 ul({className:'menu vertical'},
-                    li({className:'query'},s.query),
-                    ...s.results.map( function(v) {
+                    li({className:'query'},query),
+                    ...results.map( function(v) {
                         return li(a({href: v['href']},v['label']))
                     })
                 )
