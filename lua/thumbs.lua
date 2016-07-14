f4b8475f0256faea534663a32dfb869a4d1894b0
Brian Duggan <bduggan@matatu.org>
2016-07-14 11:13:48 -0400

    wip

--- lua/thumbs.lua
+++ lua/thumbs.lua
@@ -1,15 +1,15 @@
 
--- http://leafo.net/posts/creating_an_image_server.html
+function log(msg)
+    ngx.log(ngx.STDERR, msg)
+end
 
-ngx.log(ngx.STDERR, "in thumbs.lua")
-ngx.log(ngx.STDERR, "root is " .. ngx.root);
+local sig, path, ext = ngx.var.sig, ngx.var.path, ngx.var.ext
+local root = os.getenv('UTIAJI_HOME') .. '/static/pim'
 
-local sig, size, path, ext =
-  ngx.var.sig, ngx.var.size, ngx.var.path, ngx.var.ext
+log('path is ' .. path)
 
-local secret = "ioiasdfjj" -- signature secret key
-local images_dir = "static/pim/up/" -- where images come from
-local cache_dir = "static/pim/cache/" -- where images are cached
+local images_dir = root .. "/up/" -- where images come from
+local cache_dir = root .. "/cache/" -- where images are cached
 
 local function return_not_found(msg)
   ngx.status = ngx.HTTP_NOT_FOUND
@@ -18,35 +18,18 @@ local function return_not_found(msg)
   ngx.exit(0)
 end
 
-local function calculate_signature(str)
-  return ngx.encode_base64(ngx.hmac_sha1(secret, str))
-    :gsub("[+/=]", {["+"] = "-", ["/"] = "_", ["="] = ","})
-    :sub(1,12)
-end
-
---if calculate_signature(size .. "/" .. path) ~= sig then
---  return_not_found("invalid signature")
---end
-
 local source_fname = images_dir .. path
-
--- make sure the file exists
 local file = io.open(source_fname)
 
 if not file then
-  ngx.log(ngx.STDERR, "not found: " .. source_fname)
+  ngx.log(ngx.STDERR, "source not found: " .. source_fname)
   return_not_found()
 end
-
 file:close()
 
-local dest_fname = cache_dir .. ngx.md5(size .. "/" .. path) .. "." .. ext
-
--- resize the image
+local dest_fname = cache_dir .. path .. ".png"
 local magick = require("magick")
 
 ngx.log(ngx.STDERR, "generating thumbnail: " .. dest_fname)
-
-magick.thumb(source_fname, size, dest_fname)
-
+magick.thumb(source_fname, '150x150', dest_fname)
 ngx.exec(ngx.var.request_uri)
