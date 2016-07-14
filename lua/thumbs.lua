23419ee86b2d633b76b8eeae93de01630dc34ecc
Brian Duggan <bduggan@matatu.org>
2016-07-14 11:24:06 -0400

    wip

--- lua/thumbs.lua
+++ lua/thumbs.lua
@@ -1,35 +1,20 @@
 
-function log(msg)
-    ngx.log(ngx.STDERR, msg)
-end
-
-local sig, path, ext = ngx.var.sig, ngx.var.path, ngx.var.ext
+local path = ngx.var.path
 local root = os.getenv('UTIAJI_HOME') .. '/static/pim'
+local src = root .. '/up/' .. path
+local dst = root .. '/cache/' .. path .. '.png'
 
-log('path is ' .. path)
-
-local images_dir = root .. "/up/" -- where images come from
-local cache_dir = root .. "/cache/" -- where images are cached
-
-local function return_not_found(msg)
-  ngx.status = ngx.HTTP_NOT_FOUND
-  ngx.header["Content-type"] = "text/html"
-  ngx.say(msg or "not found")
-  ngx.exit(0)
-end
-
-local source_fname = images_dir .. path
-local file = io.open(source_fname)
+local file = io.open(src)
 
 if not file then
-  ngx.log(ngx.STDERR, "source not found: " .. source_fname)
-  return_not_found()
+  ngx.log(ngx.STDERR, "source not found: " .. src)
+  ngx.status = ngx.HTTP_NOT_FOUND
+  ngx.say("not found")
+  ngx.exit(0)
 end
 file:close()
 
-local dest_fname = cache_dir .. path .. ".png"
+ngx.log(ngx.STDERR, "generating thumbnail: " .. dst)
 local magick = require("magick")
-
-ngx.log(ngx.STDERR, "generating thumbnail: " .. dest_fname)
-magick.thumb(source_fname, '150x150', dest_fname)
+magick.thumb(src, '150x150', dst)
 ngx.exec(ngx.var.request_uri)
